dc86c1e472695cd60036485ccdbb98b113b8652c
Faruk5456 <98277675+Faruk5456@users.noreply.github.com>
2023-04-08 19:45:23 +0300

    Ayaz

--- screencast/308-create-bep20-token-bsc/Token.sol
+++ screencast/308-create-bep20-token-bsc/Token.sol
@@ -41,4 +41,6 @@ contract Token {
         emit Approval(msg.sender, spender, value);
         return true;   
     }
-}
+}Warning: SPDX license identifier not provided in source file. Before publishing, consider adding a comment containing "SPDX-License-Identifier: <SPDX-License>" to each source file. Use "SPDX-License-Identifier: UNLICENSED" for non-open-source code. Please see https://spdx.org for more information.
+--> Ayazo.sol
+
